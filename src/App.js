3cfb49f35705b756ebdbb03686cedd617313ec0b
akalinoxd <jvitorcavalcante18@gmail.com>
2024-03-15 12:03:29 -0300

    testing commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <>
       <main>
-        <table>
+        <table className='gameBoard'>
           <tr>
             <td></td>
             <td></td>
